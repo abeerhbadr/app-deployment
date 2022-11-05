32adf8e6746b5941469633ed83731e5ebc5775db
Abeerhbadr <abeer.h.badr@gmail.com>
2022-11-05 11:12:38 +0200

    doumentation

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,1 +1,5 @@
-echo hello world
+eb init udagram-api --region us-east-1 --platform node.js &&
+eb create --sample Udagramapi-env
+eb use Udagramapi-env &&
+eb setenv POSTGRES_HOST=database-1.cc5377s4kcuu.us-east-1.rds.amazonaws.com POSTGRES_DB=postgres POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=myPassword &&
+eb deploy || eb deploy Udagramapi-env
