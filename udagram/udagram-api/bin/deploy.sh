1af7034158689ac7bd80200c2b7083bb88b22972
Abeerhbadr <abeer.h.badr@gmail.com>
2022-11-05 17:35:51 +0200

    deploy

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,5 +1,5 @@
 eb init udagram-api --region us-east-1 --platform node.js &&
 eb create --sample Udagramapi-env
 eb use Udagramapi-env &&
-eb setenv POSTGRES_HOST=database-1.cc5377s4kcuu.us-east-1.rds.amazonaws.com POSTGRES_DB=postgres POSTGRES_USERNAME=postgres POSTGRES_PASSWORD=myPassword &&
+eb setenv JWT_SECRET=$JWT_SECRET POSTGRES_HOST=$POSTGRES_HOST POSTGRES_DB=$POSTGRES_DB POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD &&
 eb deploy || eb deploy Udagramapi-env
