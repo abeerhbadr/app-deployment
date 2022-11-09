359c090d491b4418e1c3d2f09b49efc6acb4394c
Abeerhbadr <abeer.h.badr@gmail.com>
2022-11-09 22:36:33 +0200

    frontend apiHost

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://Udagramapi-env.eba-7fzr4vyh.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagramapi-env.eba-mfjtiy8x.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
